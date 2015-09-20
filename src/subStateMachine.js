b98fa371a05c235fa7ebd70b88777474d089ad3c
Stephen Fortune <steve.fortune@icecb.com>
2015-09-20 18:33:04 +0100

    feature(): Built initial state registration methods

--- src/subStateMachine.js
+++ src/subStateMachine.js
@@ -4,5 +4,38 @@ angular
   .service('$subStateMachine', $subStateMachine);
 
 function $subStateMachine() {
-  return {};
+
+  var createStateConf = function(sub) {
+    var conf;
+    if (angular.isArray(sub) || angular.isFunction(sub)) {
+      conf = { autorun: sub };
+    } else if (angular.isObject(sub)) {
+      conf = {
+        name: sub.name,
+        params: sub.params
+      };
+    } else {
+      conf = { name: sub };
+    }
+    return conf;
+  };
+
+  return {
+    subStates: {},
+    state: function(stateName, sub) {
+      var args = Array.prototype.slice.call(arguments);
+      var self = this;
+      var subConf;
+      if (args.length > 2) {
+        var subConfs = args.slice(1);
+        subConf = _.map(subConfs, createStateConf);
+      } else {
+        subConf = createStateConf(sub);
+      }
+      this.subStates[stateName] = subConf;
+    },
+    get: function(stateName) {
+      return this.subStates[stateName];
+    }
+  };
 }
--- test/subStateMachine.spec.js
+++ test/subStateMachine.spec.js
@@ -9,13 +9,62 @@ describe("$subStateMachine", function() {
     $subStateMachine = _$subStateMachine_;
   }));
 
-  describe("state", function() {
+  describe('.get', function() {
 
-    it("should register simple subscription for state", function() {
+    it("should get registered subscription conf object", function() {
       $subStateMachine.state('routeName', 'subName');
-      expect($subStateMachine.get('routeName')).toBe({
-        name: 'subName'
+      expect(angular.isObject($subStateMachine.get('routeName'))).toBe(true);
+    });
+
+    it("should get array of registered subscription conf objects", function() {
+      $subStateMachine.state('routeName', 'subName', 'anotherSub', 'yetAnother');
+      expect(angular.isArray($subStateMachine.get('routeName'))).toBe(true);
+    });
+
+  });
+
+  describe(".state", function() {
+
+    it("should register simple subscription conf", function() {
+      $subStateMachine.state('routeName', 'subName');
+      var subState = $subStateMachine.get('routeName');
+      expect(subState.name).toBe('subName');
+    });
+
+    it("should register subscription conf with params", function() {
+      $subStateMachine.state('routeName', {
+        name: 'subName',
+        params: ['routeParam1', 'routeParam2']
       });
+      var subState = $subStateMachine.get('routeName');
+      expect(subState.name).toBe('subName');
+      expect(subState.params).toEqual(['routeParam1', 'routeParam2']);
+    });
+
+    it("should register subscription conf with autorun computation", function() {
+      var block = function($meteor) {
+        $meteor.subscribe('something', Session.get('reactiveParam'));
+      };
+      $subStateMachine.state('routeName', ['$meteor', block]);
+      var subState = $subStateMachine.get('routeName');
+      expect(subState.autorun).toEqual(['$meteor', block]);
+    });
+
+    it("should register array of subscription configurations", function() {
+      var block = function($meteor) {
+        $meteor.subscribe('something', Session.get('reactiveParam'));
+      };
+      $subStateMachine.state('routeName', 'sub1', 'sub2', {
+        name: 'sub3',
+        params: ['one', 'two']
+      }, ['$meteor', block]);
+      var states = $subStateMachine.get('routeName');
+      expect(states).toEqual([
+        { name: 'sub1' },
+        { name: 'sub2' },
+        { name: 'sub3', params: ['one', 'two'] },
+        { autorun: ['$meteor', block] }
+      ]);
     });
 
   });
