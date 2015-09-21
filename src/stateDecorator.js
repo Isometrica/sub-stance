7b62d9dc81e6116e1efeb748bf27a378ab387189
Stephen Fortune <steve.fortune@icecb.com>
2015-09-21 19:20:37 +0100

    feature(ui.router): First attempt at state decorator

---
+++ src/stateDecorator.js
@@ -0,0 +1,73 @@
+
+angular
+  .module('isa.substance')
+  .config(stateProviderDecorator);
+
+// Approach 1:
+//
+// Decoration:
+// - Decorate builder's data function to merge the subs arrays with parents
+// Trigger transitions:
+// - In $stateChangeStart, append transition recipie to resolves.
+// - Also ensure that other resolves come after it, i.e. depend on it
+//
+// Note: https://github.com/angular-ui/ui-router/issues/1165 you need
+// resolve: {} to be able to append them in the event handler.
+
+function decorateStateProvider($stateProvider, $rootScope) {
+
+  var subResolveKey = "$__subs";
+
+  function dataDecorateFn(state, parentFn) {
+
+    if (state.parent && state.parent.data) {
+      var pData = state.parent.data;
+      if (pData.$subs) {
+        if (!state.data) {
+          state.data = {};
+        }
+        if (state.data.subs) {
+          state.data.$subs = pData.$subs.concat(state.data.$subs);
+        } else {
+          state.data.$subs = pData.$subs;
+        }
+      }
+    }
+    parentFn(state);
+
+  }
+
+  function ensureSubs(e, toState, toParams, fromState, fromParams) {
+
+    if (!toState.resolve) {
+      throw new Error("No resolve table.");
+    }
+
+    _.each(toState.resolve, function(resolve) {
+      if (_.isArray(resolve)) {
+        resolve.splice(resolve.count - 2, 0, subResolveKey);
+      } else if (_.isFunction(resolve)) {
+        if (!resolve.$inject) {
+          resolve.$inject = [];
+        }
+        resolve.$inject.push(subResolveKey);
+      }
+    });
+
+    toState.resolve[subResolveKey] = ['$subs', function($subs) {
+      return $subs.transition(toState.name, toParams);
+    }];
+
+  }
+
+  $stateProvider.decorate('data', dataDecorateFn);
+  $rootScope.$on('$stateChangeStart', ensureSubs);
+
+}
+decorateStateProvider.$inject = ['$stateProvider']
+
+// Approach 2:
+//
+// - Wrap $stateProvider.state
+// - Add add the resolves in there
+// - How do we guarentee merge sub conf with parent?
